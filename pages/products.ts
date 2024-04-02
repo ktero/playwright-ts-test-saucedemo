189306aa00d33a2c5e24db8631d1d589393c908e
Kenneth Tero <ktero.dev@gmail.com>
2024-04-03 00:31:32 +0800

    add optional action to add item from product item page

--- pages/products.ts
+++ pages/products.ts
@@ -6,6 +6,7 @@ export class ProductsPage extends NavigationBar {
 
     // Define locators
     readonly productItem: Locator;
+    readonly productItemName: Locator;
 
     constructor(page: Page) {
         super(page);
@@ -13,9 +14,10 @@ export class ProductsPage extends NavigationBar {
 
         // Initialize locators
         this.productItem = this.page.locator('[data-test="inventory-item"]');
+        this.productItemName = this.page.locator('[data-test="inventory-item-name"]');
     }
 
-    async performAddItemsToCart(itemToAdd: string | string[]) {
+    async performAddItemsToCart(itemToAdd: string | string[], addItemsFromProductItemPage: boolean = false) {
         
         let items: string[] = typeof itemToAdd === 'string' ? [itemToAdd] : [...itemToAdd];
         let itemName: string;
@@ -24,11 +26,19 @@ export class ProductsPage extends NavigationBar {
         // Loop to add all given items to the shopping cart
         for (let i: number = 0; i < items.length; i++) {
             itemName = items[i];
+            if (addItemsFromProductItemPage) {
+                // Go to product item page
+                await this.clickProductItemName(itemName);
+            }
             await this.addItemToCart(itemName);
             itemCounter++;
             // Assert total number of items added in the shopping cart
             await this.assertNumberBesideCartIcon(itemCounter);
         }
+    }
+
+    private async clickProductItemName(itemName: string) {
+        await this.productItemName.filter({ hasText: itemName }).click();
     }
 
     private async addItemToCart(itemName: string) {
