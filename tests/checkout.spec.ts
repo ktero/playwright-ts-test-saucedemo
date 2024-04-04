d27a887a4add2ff277a9d23bf3b003b96364241e
Kenneth Tero <ktero.dev@gmail.com>
2024-04-05 01:35:12 +0800

    update scripts to use the newly created fixture (text-fixture)

--- tests/checkout.spec.ts
+++ tests/checkout.spec.ts
@@ -1,25 +1,11 @@
-import { test } from '@playwright/test';
-
-import { LoginPage } from '../pages/login';
-import { ProductsPage } from '../pages/products';
-import { CartPage } from '../pages/cart';
-import { CheckoutInformationPage } from '../pages/checkout-information';
-import { CheckoutOverviewPage } from '../pages/checkout-overview';
-import { CheckoutCompletePage } from '../pages/checkout-complete';
+import { test } from '../fixtures/test-fixture';
 
 import * as loginData from '../data/login.json';
 import * as testData from '../data/test.json';
 
 test.describe(`Checkout`, () => {
   
-  test('should be able to checkout multiple items', async ({ page }) => {
-
-    const loginPage: LoginPage = new LoginPage(page);
-    const productsPage: ProductsPage = new ProductsPage(page);
-    const cartPage: CartPage = new CartPage(page);
-    const checkoutInformationPage: CheckoutInformationPage = new CheckoutInformationPage(page);
-    const checkoutOverviewPage: CheckoutOverviewPage = new CheckoutOverviewPage(page);
-    const checkoutCompletePage: CheckoutCompletePage = new CheckoutCompletePage(page);
+  test('should be able to checkout multiple items', async ({ loginPage, productsPage, cartPage, checkoutInformationPage, checkoutOverviewPage, checkoutCompletePage }) => {
 
     // Test data below
     // Login
@@ -39,7 +25,7 @@ test.describe(`Checkout`, () => {
     const expectedCompleteHeader: string = testData.Test1.ExpectedCheckoutCompleteMessage.Header;
     const expectedCompleteText: string = testData.Test1.ExpectedCheckoutCompleteMessage.Text;
   
-    await loginPage.goto();
+    // await loginPage.goto();
     await loginPage.performLogin(username, password, isValidLogin);
 
     await productsPage.performAddItemsToCart(items);
@@ -58,14 +44,7 @@ test.describe(`Checkout`, () => {
     await checkoutCompletePage.navigateToProductsPage();
   });
 
-  test('should be able to remove items in shopping cart and then checkout the remaining items', async ({ page }) => {
-
-    const loginPage: LoginPage = new LoginPage(page);
-    const productsPage: ProductsPage = new ProductsPage(page);
-    const cartPage: CartPage = new CartPage(page);
-    const checkoutInformationPage: CheckoutInformationPage = new CheckoutInformationPage(page);
-    const checkoutOverviewPage: CheckoutOverviewPage = new CheckoutOverviewPage(page);
-    const checkoutCompletePage: CheckoutCompletePage = new CheckoutCompletePage(page);
+  test('should be able to remove items in shopping cart and then checkout the remaining items', async ({ loginPage, productsPage, cartPage, checkoutInformationPage, checkoutOverviewPage, checkoutCompletePage }) => {
     
     // Test data below
     // Login
@@ -108,13 +87,7 @@ test.describe(`Checkout`, () => {
     await checkoutCompletePage.navigateToProductsPage();
   });
 
-  test('should be able to checkout item if added from item page', async ({ page }) => {
-    const loginPage: LoginPage = new LoginPage(page);
-    const productsPage: ProductsPage = new ProductsPage(page);
-    const cartPage: CartPage = new CartPage(page);
-    const checkoutInformationPage: CheckoutInformationPage = new CheckoutInformationPage(page);
-    const checkoutOverviewPage: CheckoutOverviewPage = new CheckoutOverviewPage(page);
-    const checkoutCompletePage: CheckoutCompletePage = new CheckoutCompletePage(page);
+  test('should be able to checkout item if added from item page', async ({ loginPage, productsPage, cartPage, checkoutInformationPage, checkoutOverviewPage, checkoutCompletePage }) => {
     
     // Test data below
     // Login
@@ -154,12 +127,7 @@ test.describe(`Checkout`, () => {
     
   });
 
-  test('should not be able to checkout if checkout information is not given', async ({ page }) => {
-
-    const loginPage: LoginPage = new LoginPage(page);
-    const productsPage: ProductsPage = new ProductsPage(page);
-    const cartPage: CartPage = new CartPage(page);
-    const checkoutInformationPage: CheckoutInformationPage = new CheckoutInformationPage(page);
+  test('should not be able to checkout if checkout information is not given', async ({ loginPage, productsPage, cartPage, checkoutInformationPage, checkoutOverviewPage, checkoutCompletePage }) => {
 
     // Test data below
     // Login
