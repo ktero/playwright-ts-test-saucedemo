13ce851b6f118b9d82e5b75dbc4dbc54393a4d6e
Kenneth Tero <ktero.dev@gmail.com>
2024-05-11 11:16:28 +0800

    remove isValidLogin as it is not needed anymore, and use the new login assertions

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -8,20 +8,20 @@ test.describe(`Login`, () => {
 
     const username: string = loginData.ValidScenario.Username;
     const password: string = loginData.ValidScenario.Password;
-    const isValidLogin: boolean = loginData.ValidScenario.IsValidLogin;
 
-    await loginPage.performLogin(username, password, isValidLogin);
+    await loginPage.performLogin(username, password);
+    await loginPage.assertLoginIsSuccessful();
   });
 
   test('should not be able to login when inputting invalid credentials', async ({ loginPage }) => {
 
     const username: string = loginData.InvalidScenario.Username;
     const password: string = loginData.InvalidScenario.Password;
-    const isValidLogin: boolean = loginData.InvalidScenario.IsValidLogin;
     const expectedErrorMessage: string = loginData.InvalidScenario.ExpectedErrorMessage;
     
-    await loginPage.performLogin(username, password, isValidLogin);
+    await loginPage.performLogin(username, password);
 
+    await loginPage.assertLoginIsUnsuccessful();
     await loginPage.assertLoginErrorIsDisplay();
     await loginPage.assertActualLoginErrorMessageMatchesExpectedErrorMessage(expectedErrorMessage);
   });
@@ -30,11 +30,11 @@ test.describe(`Login`, () => {
 
     const username: string = loginData.InvalidScenario_NoInput.Username;
     const password: string = loginData.InvalidScenario_NoInput.Password;
-    const isValidLogin: boolean = loginData.InvalidScenario_NoInput.IsValidLogin;
     const expectedErrorMessage: string = loginData.InvalidScenario_NoInput.ExpectedErrorMessage;
     
-    await loginPage.performLogin(username, password, isValidLogin);
+    await loginPage.performLogin(username, password);
 
+    await loginPage.assertLoginIsUnsuccessful();
     await loginPage.assertLoginErrorIsDisplay();
     await loginPage.assertActualLoginErrorMessageMatchesExpectedErrorMessage(expectedErrorMessage);
   });
@@ -43,11 +43,11 @@ test.describe(`Login`, () => {
 
     const username: string = loginData.InvalidScenario_NoUsername.Username;
     const password: string = loginData.InvalidScenario_NoUsername.Password;
-    const isValidLogin: boolean = loginData.InvalidScenario_NoUsername.IsValidLogin;
     const expectedErrorMessage: string = loginData.InvalidScenario_NoUsername.ExpectedErrorMessage;
     
-    await loginPage.performLogin(username, password, isValidLogin);
+    await loginPage.performLogin(username, password);
 
+    await loginPage.assertLoginIsUnsuccessful();
     await loginPage.assertLoginErrorIsDisplay();
     await loginPage.assertActualLoginErrorMessageMatchesExpectedErrorMessage(expectedErrorMessage);
   });
@@ -56,11 +56,11 @@ test.describe(`Login`, () => {
 
     const username: string = loginData.InvalidScenario_NoPassword.Username;
     const password: string = loginData.InvalidScenario_NoPassword.Password;
-    const isValidLogin: boolean = loginData.InvalidScenario_NoPassword.IsValidLogin;
     const expectedErrorMessage: string = loginData.InvalidScenario_NoPassword.ExpectedErrorMessage;
     
-    await loginPage.performLogin(username, password, isValidLogin);
+    await loginPage.performLogin(username, password);
 
+    await loginPage.assertLoginIsUnsuccessful();
     await loginPage.assertLoginErrorIsDisplay();
     await loginPage.assertActualLoginErrorMessageMatchesExpectedErrorMessage(expectedErrorMessage);
   });
@@ -69,11 +69,11 @@ test.describe(`Login`, () => {
 
     const username: string = loginData.InvalidScenario_LockedOutUser.Username;
     const password: string = loginData.InvalidScenario_LockedOutUser.Password;
-    const isValidLogin: boolean = loginData.InvalidScenario_LockedOutUser.IsValidLogin;
     const expectedErrorMessage: string = loginData.InvalidScenario_LockedOutUser.ExpectedErrorMessage;
     
-    await loginPage.performLogin(username, password, isValidLogin);
+    await loginPage.performLogin(username, password);
 
+    await loginPage.assertLoginIsUnsuccessful();
     await loginPage.assertLoginErrorIsDisplay();
     await loginPage.assertActualLoginErrorMessageMatchesExpectedErrorMessage(expectedErrorMessage);
   });
